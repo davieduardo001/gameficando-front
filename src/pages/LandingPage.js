0168385cb6d6da4f95544846cd7b8965ea10963e
davieduardo001 <ed.miranda@tutanota.com>
2024-09-04 11:46:18 -0400

    update fonts

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,6 +7,8 @@ import logoImage from '../assets/images/logo_inicial.png';
 const LandingPage = () => {
   return (
     <div>
+
+      {/* primeiro bloco */}
       <Block backgroundImage="fundo_bloco_1.png">
         
         <img src={logoImage} className="logo-incial" alt="Logo"/>
@@ -14,7 +16,16 @@ const LandingPage = () => {
 
       </Block>
 
-      <Block backgroundColor="#f8b400">Block 1 (Cor de fundo)</Block>
+      {/* segundo bloco */}
+      <Block backgroundColor="#fff">
+        
+        <h2 className='h2-mine-brown'>Benefícios Incluídos no Curso</h2>
+        
+        {/* <CardSimples image="" title="" text=""></CardSimples> */}
+
+      </Block>
+
+
       <Block backgroundColor="#28a745">Block 3 (Cor de fundo)</Block>
       <Block>Block 4 (Sem fundo, cor padrão branca)</Block>
       <Block backgroundImage="fundo_bloco_1.png">Block 5 (Imagem de fundo)</Block>
